444fcc897e5ad5ee727f26bfb8f6907a50c2740f
João Vitor Retamero <joaovretamero@gmail.com>
2024-02-15 13:32:13 -0300

    Base bottom sheet widget

---
+++ lib/components/team_sheet.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TeamSheet extends StatefulWidget {
+  const TeamSheet({super.key});
+
+  @override
+  State<TeamSheet> createState() => _TeamSheetState();
+}
+
+class _TeamSheetState extends State<TeamSheet> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
